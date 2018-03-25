70daff3ce55bf9c89a73c37db2fdc5a04665c377
Ozan Çetin <ozanc3tin@gmail.com>
2018-03-25 21:54:51 +0300

    Add files via upload

---
+++ 2018/Securinets CTF Quals 2018/looser/solving.py
@@ -0,0 +1,16 @@
+import binascii
+filename = 'flag.png.crypt'
+with open(filename, 'rb') as f:
+    content = f.read()
+XORdata = binascii.hexlify(content)
+
+cipher = (XORdata).decode('hex')
+
+for i in range(0x00,0xff):
+	result = ""
+	for j in cipher:
+		result += chr(i^ord(j))
+	if 'PNG' in result:
+		solved = open("flag.png","w")
+		solved.write(result)
+		solved.close()
